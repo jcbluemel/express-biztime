2a555c7b76a349212e5572fc9d62a0600a980188
Joel <jcbluemel@yahoo.com>
2022-06-30 12:27:46 -0700

    added put and delete routes

--- routes/companies.js
+++ routes/companies.js
@@ -17,8 +17,9 @@ router.get("/", async function (req, res, next) {
 });
 
 
-/** GET / - details about single company by code
- *  returns `{ company: {code, name, description } }` */
+/** GET / - Details about single company by code
+ *  Returns 404 if company code not found.
+ *  Else returns `{ company: {code, name, description } }` */
 
 router.get("/:code", async function (req, res, next) {
     const code = req.params.code;
@@ -28,6 +29,8 @@ router.get("/:code", async function (req, res, next) {
             WHERE code = $1`, [code]);
 
     const company = result.rows[0];
+    if (!company) throw new NotFoundError(`No matching company: ${code}`);
+
     return res.json({ company: company });
 });
 
@@ -38,15 +41,68 @@ router.get("/:code", async function (req, res, next) {
 router.post("/", async function (req, res, next) {
     const { code, name, description } = req.body;
     const result = await db.query(
-        `INSERT INTO companies (code, name, description)
+        `INSERT INTO companies
+            (code, name, description)
             VALUES ($1, $2, $3)
             RETURNING code, name, description`,
         [code, name, description]
     );
 
     const newCompany = result.rows[0];
+
     return res.status(201).json({ company: newCompany });
 });
+
+
+/** PUT / - Edit an existing company, takes JSON: { name, description }
+ *  Returns 404 if company code not found.
+ *  Else returns `{ company: { code, name, description } }` */
+
+router.put("/:code", async function (req, res, next) {
+
+    if ("code" in req.body) throw new BadRequestError("Not allowed");
+
+    const { name, description } = req.body;
+    const code = req.params.code;
+
+    const result = await db.query(
+        `UPDATE companies
+            SET name = $1,
+                description = $2
+            WHERE code = $3
+            RETURNING code, name, description`,
+        [name, description, code]
+    );
+
+    const editedCompany = result.rows[0];
+    if (!editedCompany) throw new NotFoundError(`No matching company: ${code}`);
+
+    return res.json({ company: editedCompany });
+});
+
+
+
+/** DELETE / - Delete a company
+ *  Returns 404 if company code not found.
+ *  Else returns `{ status: "deleted" }` */
+
+router.delete("/:code", async function (req, res, next) {
+
+    const code = req.params.code;
+
+    const result = await db.query(
+        `DELETE FROM companies
+            WHERE code = $1
+            RETURNING code`,
+        [code]
+    );
+
+    const deletedCompanyCode = result.rows[0];
+    if (!deletedCompanyCode) throw new NotFoundError(`No matching company: ${code}`);
+
+    return res.json({ status: "deleted" });
+});
+
 
 
 module.exports = router;
